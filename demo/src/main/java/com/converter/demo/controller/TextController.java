8c1e207bb8faf191fcc053f4c8d9950b67903765
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-27 21:09:30 +0400

    New method added and endpoint changed!

--- demo/src/main/java/com/converter/demo/controller/TextController.java
+++ demo/src/main/java/com/converter/demo/controller/TextController.java
@@ -1,13 +1,16 @@
 package com.converter.demo.controller;
 
 import com.converter.demo.service.PDFConverterService;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Objects;
 
+@CrossOrigin(origins = "*")
 @RestController
+@RequestMapping("/api/v1/converter/upload")
 public class TextController {
 
     private final PDFConverterService pdfConverterService;
@@ -17,7 +20,7 @@ public class TextController {
     }
 
 
-    @PostMapping("/convert")
+    @PostMapping("/text-to-pdf")
     public ResponseEntity<byte[]> convertTxtToPdf(@RequestParam("file") MultipartFile file) {
         if (file.isEmpty() || !Objects.requireNonNull(file.getOriginalFilename()).endsWith(".txt")) {
             return ResponseEntity.badRequest().body(null);
@@ -25,5 +28,17 @@ public class TextController {
 
         return pdfConverterService.convertTxtToPdf(file);
     }
+
+    @PostMapping("/pdf-to-txt")
+    public ResponseEntity<Resource> convertPdfToTxt(@RequestParam("file") MultipartFile file) {
+
+        if (file.isEmpty() || !Objects.requireNonNull(file.getOriginalFilename()).toLowerCase().endsWith(".pdf")) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        return pdfConverterService.convertPdfToTxt(file);
+    }
+
+
 
 }
